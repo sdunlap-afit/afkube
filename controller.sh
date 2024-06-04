a59ead238923e3424c025181623f691d0fbc0f07
sdunlap <sdunlap42@gmail.com>
2024-06-04 10:11:18 -0400

    Basic setup

---
+++ controller.sh
@@ -0,0 +1,4 @@
+
+echo "Run this on the controller node after setup.sh"
+
+microk8s enable registry
---
+++ setup.sh
@@ -0,0 +1,18 @@
+
+
+sudo apt update && sudo apt upgrade -y
+
+sudo snap install microk8s --classic
+
+##### Enable necessary features #####
+
+sudo sed -i '${s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1/}' /boot/firmware/cmdline.txt
+
+
+##### Add user to microk8s group #####
+
+sudo usermod -a -G microk8s $USER
+mkdir -p ~/.kube
+sudo chown -f -R $USER ~/.kube
+
+sudo reboot
